7fd3c156dd8ead38a6690f81254322925ba8283d
cahyafaisalreza <cf.reza07@gmail.com>
2021-07-12 16:16:04 +0700

    using request_cookies

--- django/djangodev/apps/core/views.py
+++ django/djangodev/apps/core/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def setcookie(request):
+    html = HttpResponse("<h1>Dataflair Django Tutorial</h1>")
+    html.set_cookie('dataflair', 'Hello this is your cookies', max_age = None)
+    return html
+
+# read cookies from request
+# using request.COOKIE[]
+def showcookie(request):
+    show = request.COOKIES['dataflair']
+    html = "<center> New Page <br>{0}</center>".format(show)
+    return HttpResponse(html)
