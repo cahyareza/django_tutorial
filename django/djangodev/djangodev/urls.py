2d52aaf45fc8de44469c155e57ff15e5cac662dc
cahyafaisalreza <cf.reza07@gmail.com>
2021-07-21 15:18:35 +0700

    file_upload

--- django/djangodev/djangodev/urls.py
+++ django/djangodev/djangodev/urls.py
@@ -15,7 +15,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from . import settings
+from django.conf.urls.static import static
+
+from apps.profile_maker.views import create_profile
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('upload/', create_profile, name = "create_profile"),
 ]
+
+if settings.DEBUG:
+    # urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
